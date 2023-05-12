2e10ec2ab6fb05f483e54e93026e92ee68673936
haakonsollund <113894363+haakonsollund@users.noreply.github.com>
2023-05-12 08:25:10 +0200

    asdad

--- html/gamble.js
+++ html/gamble.js
@@ -5,6 +5,7 @@ var EL_moneyButton = document.querySelector('#dollar_img')
 var EL_money = document.querySelector('#money_amount')
 var EL_bet100 = document.querySelector('#bet100')
 var randomtall = Math.floor(Math.random () * 100);
+var EL_bet200 = document.querySelector('#bet200')
 
 
 
@@ -21,22 +22,49 @@ function clickmoney(){
 function bet100(){
     if (money>100){
         money = money - 100;
+        EL_money.innerHTML = money 
+        randomtall = Math.floor(Math.random () * 10);
+    }
+    randittall();
+}
+
+function randittall(){
+    
+    randomtall.innerHTML = randomtall
+    console.log(randomtall);
+    if(randomtall == 7){
+        money = money + 300;
         EL_money.innerHTML = money
-        
-        
     }
 
 }
 
-function randittall(){
-    randomtall = Math.floor(Math.random () * 10);
-    randomtall.innerHTML = randomtall
 
+function randittall2(){
+    randomtall.innerHTML = randomtall
+    console.log(randomtall);
+    if(randomtall == 2){
+        money = money + 500
+        EL_money.innerHTML = money
+    }
+
+}
+
+function bet200(){
+    if (money>200){
+        money = money - 200;
+        EL_money.innerHTML = money
+        randomtall = Math.floor(Math.random () * 10);
+
+        
+    }
+    randittall2()
 }
 
 
 EL_bet100.addEventListener('click', bet100)
 EL_moneyButton.addEventListener('click', clickmoney)
+EL_bet200.addEventListener('click',bet200)
 
 
 
