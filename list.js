969b9832d9b14ce0e19b00cc84342050c5db0e0b
haakonsollund <113894363+haakonsollund@users.noreply.github.com>
2023-08-28 13:52:46 +0200

    er

---
+++ list.js
@@ -0,0 +1,18 @@
+var EL_bilmerke = document.querySelector('#bilmerkeV')
+var EL_kostnad = document.querySelector('#kostnadV')
+var EL_modell = document.querySelector('#mddellV')
+var EL_motor = document.querySelector('#motorV')
+var EL_topspeed = document.querySelector('#topspeedV')
+var saveinfo = document.querySelector('#saveinfo')
+
+
+
+
+
+function addtotable(){
+    
+
+
+}
+
+saveinfo.addEventListener('click', addtotable)
