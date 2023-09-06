a5acf1eaf9356ccb9dc310fb578066cd638ee2ea
haakonsollund <113894363+haakonsollund@users.noreply.github.com>
2023-09-06 10:15:50 +0200

    asdasd
    as

--- list.js
+++ list.js
@@ -1,6 +1,7 @@
 var inputs = document.querySelectorAll("input[type='text']")
 var saveinfo = document.querySelector('#saveinfo')
 var table = document.querySelector('#tbody')
+var DeleteButton = document.querySelector('#btn')
 
 console.log(inputs)
 
@@ -25,9 +26,17 @@ function save_function(){
 
     console.log(tr)
 }
-    
 
+for(var i = 0; i < DeleteButton.length; i ++){
+    
+}
+    
+function removeitem(){
+    this.parentNode.remove();
+    console.log("delete button clicked")
+}
 
 
 
 saveinfo.addEventListener("click", save_function)
+DeleteButton[i].addEventListener("click", removeitem);
