eb873db782e314372fc6f99105d0f809852033d2
haakonsollund <113894363+haakonsollund@users.noreply.github.com>
2023-09-13 11:32:53 +0200

    sdfg,.

--- list.js
+++ list.js
@@ -2,6 +2,7 @@ var inputs = document.querySelectorAll("input[type='text']")
 var saveinfo = document.querySelector('#saveinfo')
 var table = document.querySelector('#tbody')
 var DeleteButton = document.getElementsByClassName("btn")
+var tr_rows = document.getElementsByTagName("tr");
 
 console.log(inputs)
 
@@ -31,10 +32,16 @@ function save_function(){
     deleteButton.textContent = "Delete";
     deleteButton.addEventListener("click", removeItem);
     td.appendChild(deleteButton)
-
+        
+   
     
     console.log("button clicked.")
     table.appendChild(tr);
+     var tr_rows = document.getElementsByTagName("tr");
+
+    for(var i = 0; i < tr_rows.length; i++){
+        tr_rows[i].addEventListener("click",set_inputs)
+    }
 
     console.log(tr)
 }
@@ -43,16 +50,57 @@ for(var i = 0; i < DeleteButton.length; i++){
     DeleteButton[i].addEventListener("click", removeItem); 
     
 }
+
+
+for(var i = 0; i < tr_rows.length; i++){
+    tr_rows[i].addEventListener("click",set_inputs)
+}
     
 function removeItem(){
     this.parentNode.parentNode.remove();
     console.log("delete button clicked")
 }
 
-function readTable(){
-    const table =document.getElementById('tbody') 
+
+
+function set_inputs(){
+    console.log(this)
+    var tds = this.childNodes
+    console.log(tds)
+    tds.forEach(function(node,index){
+        console.log(node,index)
+            if (index != 11){
+                node.innerHTML = "<input type='text' value='"+node.innerHTML +"'>"
+
+            }
+
+        
+    })
+    this.removeEventListener("click",set_inputs)
+
+    this.addEventListener('keypress', function (e) {
+        if (e.key ===   'Enter') {
+            this.addEventListener("click",set_inputs)
+            save_row(this)
+        }
+    });
+    
+
 }
 
+function save_row(row){
+    console.log("hello world")
+    console.log(row)
+    var tds = row.childNodes
+    tds.forEach(function(node,index){
+        console.log(node,index)
+            if (index != 11){
+                console.log(node)
+                node.innerHTML = node.children[0].value
+            }
+        
+    })
+}
 
 
 saveinfo.addEventListener("click", save_function)
